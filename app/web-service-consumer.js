143af414557b3d91e99a6de48a295de028fcfbfa
97114007 <97114007@users.noreply.github.com>
2016-10-17 01:50:03 +1100

    Update web-service-consumer.js
    Changed back to http

--- app/web-service-consumer.js
+++ app/web-service-consumer.js
@@ -7,6 +7,6 @@ WebServiceConsumer.prototype = {
 	constructor: WebServiceConsumer,
 	sendModuleRequest: function(moduleName, arguments) {
 		arguments.module = moduleName;
-		$.post("https://localhost/service-producer/RequestHandler.php", arguments, this.callback);
+		$.post("http://localhost/service-producer/RequestHandler.php", arguments, this.callback);
 	}
 };
